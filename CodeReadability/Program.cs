6c789ac0869c169e4bd050b6539caebda5a49a13
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-08-19 11:37:29 +0200

    code readability - unit 6

--- CodeReadability/Program.cs
+++ CodeReadability/Program.cs
@@ -1,22 +1,23 @@
-﻿Random dice = new Random();
+﻿/*
+    This code reverses message, counts the number of O's in the message
+    and prints the results to the console window
+*/
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+char[] messageCharacters = originalMessage.ToCharArray();
+Array.Reverse(messageCharacters);
 
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+int letterCount = 0;
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+foreach (char character in messageCharacters)
 {
-    if ((roll1 == roll2) && (roll2 == roll3))
+    if (character == 'o')
     {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
+        letterCount++;
     }
 }
+
+string newMessage = new String(messageCharacters);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
