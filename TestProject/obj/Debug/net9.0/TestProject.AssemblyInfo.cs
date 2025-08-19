d14350400709da54a1eaff79c066657e06a0ce63
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-08-19 20:30:46 +0200

    change folder directory

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429631f2b9365747852eb171b58be211dcfe1a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57bdccc2d4fb9691e97462a31bcddfaea1ca02e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
