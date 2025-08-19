e57bdccc2d4fb9691e97462a31bcddfaea1ca02e
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-08-19 20:21:32 +0200

    guided project: arrays,iteration,selection

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429631f2b9365747852eb171b58be211dcfe1a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a0b112d06a77730c3fa7ba76a50d041c0f62f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
