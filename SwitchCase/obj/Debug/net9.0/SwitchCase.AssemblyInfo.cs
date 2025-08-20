3ff9ee88cc55df90217638d4bc1ff42437673494
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-08-21 01:23:02 +0200

    Switch Case - unit 4

--- SwitchCase/obj/Debug/net9.0/SwitchCase.AssemblyInfo.cs
+++ SwitchCase/obj/Debug/net9.0/SwitchCase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8aaed77dcfacf1e3cfc6624f4055d516109bdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af92a582a8cac6b9f6b0710220b260b37201200f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
