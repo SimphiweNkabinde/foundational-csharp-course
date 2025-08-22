6e0a7638e427dba311ce853723f286aa53f9348f
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-08-22 16:08:09 +0200

    Format Strings: Exercise - Discover padding and alignment

--- FormatStrings/Program.cs
+++ FormatStrings/Program.cs
@@ -34,7 +34,7 @@ yourDiscount += $"A discount of {((price - salePrice) / price):P2}!"; //inserted
 Console.WriteLine(yourDiscount); */
 
 
-// Exercise - Explore string interpolation
+/* // Exercise - Explore string interpolation
 int invoiceNumber = 1201;
 decimal productShares = 25.4568m;
 decimal subtotal = 2750.00m;
@@ -45,4 +45,23 @@ Console.WriteLine($"Invoice Number: {invoiceNumber}");
 Console.WriteLine($"   Shares: {productShares:N3} Product");
 Console.WriteLine($"     Sub Total: {subtotal:C}");
 Console.WriteLine($"           Tax: {taxPercentage:P2}");
-Console.WriteLine($"     Total Billed: {total:C}");
+Console.WriteLine($"     Total Billed: {total:C}") */;
+
+
+// Exercise - Discover padding and alignment
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
