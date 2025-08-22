5d6c0b18433723ec2e686686d856a49779a34ac2
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-08-22 17:43:00 +0200

    Modify Content: Exercise - Use the string's IndexOf() and LastIndexOf() helper methods

--- ModifyContent/Program.cs
+++ ModifyContent/Program.cs
@@ -9,7 +9,7 @@ openingPosition += 1;
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length)); */
 
-string message = "What is the value <span>between the tags</span>?";
+/* string message = "What is the value <span>between the tags</span>?";
 
 const string openSpan = "<span>";
 const string closeSpan = "</span>";
@@ -19,4 +19,99 @@ int closingPosition = message.IndexOf(closeSpan);
 
 openingPosition += openSpan.Length;
 int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+Console.WriteLine(message.Substring(openingPosition, length)); */
+
+// Exercise - Use the string's IndexOf() and LastIndexOf() helper methods
+
+// IndexOf() and LastIndexOf
+/* string message = "hello there!";
+
+int first_h = message.IndexOf('h');
+int last_h = message.LastIndexOf('h');
+
+Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}."); */
+
+// Retrieve the last occurrence of a sub string
+/* string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
+
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length)); */
+
+/* // Retrieve all instances of substrings inside parentheses
+string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
+
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    // Note the overload of the Substring to return only the remaining 
+    // unprocessed message:
+    message = message.Substring(closingPosition + 1);
+}*/
+
+// Work with different types of symbol sets with IndexOfAny()
+/* string message = "Hello, world!";
+char[] charsToFind = { 'a', 'e', 'i' };
+
+int index = message.IndexOfAny(charsToFind);
+
+Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");  */
+
+string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// The IndexOfAny() helper method requires a char array of characters. 
+// You want to look for:
+
+char[] openSymbols = { '[', '{', '(' };
+
+// You'll use a slightly different technique for iterating through 
+// the characters in the string. This time, use the closing 
+// position of the previous iteration as the starting index for the 
+//next open symbol. So, you need to initialize the closingPosition 
+// variable to zero:
+
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+    if (openingPosition == -1) break;
+
+    string currentSymbol = message.Substring(openingPosition, 1);
+
+    // Now  find the matching closing symbol
+    char matchingSymbol = ' ';
+
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    // To find the closingPosition, use an overload of the IndexOf method to specify 
+    // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+    // Finally, use the techniques you've already learned to display the sub-string:
+
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
