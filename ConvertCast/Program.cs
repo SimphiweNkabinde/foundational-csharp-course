748260f344b7e5b028d847000694b90baee482e6
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2025-08-22 12:34:25 +0200

    Convert Cast - TryParse Method

---
+++ ConvertCast/Program.cs
@@ -0,0 +1,62 @@
+﻿/* int first = 2;
+string second = "4";
+int result = first + second;
+Console.WriteLine(result); */
+
+// Widening conversion
+/* int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}"); */
+
+// Narrowing conversion
+/* decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}"); */
+
+// Narrowing conversion
+/* decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}"); */
+
+/* // Use ToString() to convert a number to a string
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message); */
+
+/* // Convert a string to an int using the Parse() helper method
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum); */
+
+/* // Convert a string to a int using the Convert class
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result); */
+
+/* int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value2); */
+
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+if (result > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
